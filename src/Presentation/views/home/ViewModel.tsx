58662085ddf8c33e9ff773803f8e1f63e9642831
ErnestoAGEV <vevelerne@gmail.com>
2025-05-16 21:41:07 -0600

    Distribución de datos entre local y remoto , creacion de useCases Get and Save para LocalStorage

--- src/Presentation/views/home/ViewModel.tsx
+++ src/Presentation/views/home/ViewModel.tsx
@@ -1,47 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LoginAuthUseCase } from "../../../Domain/useCases/auth/LoginAuth";
+import { SaveUserUseCase } from "../../../Domain/useCases/userLocal/SaveUser";
+import {GetUserUseCase} from '../../../Domain/useCases/userLocal/GetUser';
 
 const HomeViewModel = () => {
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const [values, setValues] = useState({
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    getUserSession();
+  }, [])
+
+  const getUserSession = async() => {
+    const user = await GetUserUseCase();
+    console.log('USUARIO SESION: ' + JSON.stringify(user));
+  }
+  
 
   const onChange = (property: string, value: any) => {
     setValues({ ...values, [property]: value });
   };
 
   const login = async () => {
-    if(isValidForm()){
-    const response = await LoginAuthUseCase(values.email, values.password);
-    console.log('RESPONSE '+ JSON.stringify(response));
-    if(!response.success){
-      setErrorMessage(response.message);
+    if (isValidForm()) {
+      const response = await LoginAuthUseCase(values.email, values.password);
+      console.log("RESPONSE " + JSON.stringify(response));
+      if (!response.success) {
+        setErrorMessage(response.message);
+      } else {
+        await SaveUserUseCase(response.data);
+      }
     }
-    }
-  }
+  };
 
-  const isValidForm = ():boolean => {
-
-    if(values.email === '') {
-      setErrorMessage('Ingresa un correo electrónico');
+  const isValidForm = (): boolean => {
+    if (values.email === "") {
+      setErrorMessage("Ingresa un correo electrónico");
       return false;
     }
-    if(values.password === '') {
-      setErrorMessage('Ingresa la contraseña');
+    if (values.password === "") {
+      setErrorMessage("Ingresa la contraseña");
       return false;
     }
-    
+
     return true;
-
-  }
+  };
 
   return {
     ...values,
     onChange,
     login,
-    errorMessage
+    errorMessage,
   };
 };
 
