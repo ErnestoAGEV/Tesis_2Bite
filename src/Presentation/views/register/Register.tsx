ab4f5614afacc6181c5f6cba8cd7ef47ab26a63b
ErnestoAGEV <vevelerne@gmail.com>
2025-05-25 12:14:30 -0600

    Intento de revertir a versiones pasadas

--- src/Presentation/views/register/Register.tsx
+++ src/Presentation/views/register/Register.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React ,{useEffect} from "react";
 import {
   View,
   Text,
   Image,
   ScrollView,
   ToastAndroid,
-  TouchableOpacity,
 } from "react-native";
 import { RoundedButton } from "../../../Presentation/components/RoundedButton";
 import useViewModel from "./ViewModel";
 import { CustomTextInput } from "../../components/CustomTextInput";
 import styles from "./Styles";
-import { ModalPickImage } from "../../components/ModalPickImage";
 
 export const RegisterScreen = () => {
   const {
@@ -19,23 +17,19 @@ export const RegisterScreen = () => {
     lastname,
     phone,
     email,
-    image,
     password,
     confirmPassword,
     errorMessage,
     onChange,
     register,
-    pickImage,
-    takePhoto,
   } = useViewModel();
 
-  const [modalVisible, setModalVisible] = useState(false);
-
   useEffect(() => {
-    if (errorMessage !== "") {
-      ToastAndroid.show(errorMessage, ToastAndroid.LONG);
+    if(errorMessage !== ''){
+    ToastAndroid.show(errorMessage, ToastAndroid.LONG);
     }
-  }, [errorMessage]);
+  }, [errorMessage])
+  
 
   return (
     //Se comporta como si fuera una columna si no le damos orientacion
@@ -46,92 +40,86 @@ export const RegisterScreen = () => {
       />
 
       <View style={styles.logoContainer}>
-        <TouchableOpacity onPress={() => setModalVisible(true)}>
-          {image == "" ? (
-            <Image
-              source={require("../../../../assets/user_image.png")}
-              style={styles.logoImage}
-            />
-          ) : (
-            <Image source={{ uri: image }} style={styles.logoImage} />
-          )}
-        </TouchableOpacity>
+        <Image
+          source={require("../../../../assets/user_image.png")}
+          style={styles.logoImage}
+        />
 
         <Text style={styles.logoText}>SELECCIONE UNA IMAGEN</Text>
       </View>
 
       <View style={styles.form}>
+        
         <ScrollView>
-          <Text style={styles.formText}>REGISTRARSE</Text>
+        
+        <Text style={styles.formText}>REGISTRARSE</Text>
 
-          <CustomTextInput
-            image={require("../../../../assets/user.png")}
-            placeholder="Nombres"
-            keyboardType="default"
-            property="name"
-            onChangeText={onChange}
-            value={name}
-          />
+        <CustomTextInput
+          image={require("../../../../assets/user.png")}
+          placeholder="Nombres"
+          keyboardType="default"
+          property="name"
+          onChangeText={onChange}
+          value={name}
+        />
 
-          <CustomTextInput
-            image={require("../../../../assets/my_user.png")}
-            placeholder="Apellidos"
-            keyboardType="default"
-            property="lastname"
-            onChangeText={onChange}
-            value={lastname}
-          />
+        <CustomTextInput
+          image={require("../../../../assets/my_user.png")}
+          placeholder="Apellidos"
+          keyboardType="default"
+          property="lastname"
+          onChangeText={onChange}
+          value={lastname}
+        />
 
-          <CustomTextInput
-            image={require("../../../../assets/email.png")}
-            placeholder="Correo Electronico"
-            keyboardType="email-address"
-            property="email"
-            onChangeText={onChange}
-            value={email}
-          />
+        <CustomTextInput
+          image={require("../../../../assets/email.png")}
+          placeholder="Correo Electronico"
+          keyboardType="email-address"
+          property="email"
+          onChangeText={onChange}
+          value={email}
+        />
 
-          <CustomTextInput
-            image={require("../../../../assets/phone.png")}
-            placeholder="Telefono"
-            keyboardType="numeric"
-            property="phone"
-            onChangeText={onChange}
-            value={phone}
-          />
+        <CustomTextInput
+          image={require("../../../../assets/phone.png")}
+          placeholder="Telefono"
+          keyboardType="numeric"
+          property="phone"
+          onChangeText={onChange}
+          value={phone}
+        />
 
-          <CustomTextInput
-            image={require("../../../../assets/password.png")}
-            placeholder="Contraseña"
-            keyboardType="default"
-            property="password"
-            onChangeText={onChange}
-            value={password}
-            secureTextEntry={true}
-          />
+        <CustomTextInput
+          image={require("../../../../assets/password.png")}
+          placeholder="Contraseña"
+          keyboardType="default"
+          property="password"
+          onChangeText={onChange}
+          value={password}
+          secureTextEntry={true}
+        />
 
-          <CustomTextInput
-            image={require("../../../../assets/confirm_password.png")}
-            placeholder="Confirmar Contraseña"
-            keyboardType="default"
-            property="confirmPassword"
-            onChangeText={onChange}
-            value={confirmPassword}
-            secureTextEntry={true}
-          />
+        <CustomTextInput
+          image={require("../../../../assets/confirm_password.png")}
+          placeholder="Confirmar Contraseña"
+          keyboardType="default"
+          property="confirmPassword"
+          onChangeText={onChange}
+          value={confirmPassword}
+          secureTextEntry={true}
+        />
 
-          <View style={{ marginTop: 30 }}>
-            <RoundedButton text="CONFIRMAR" onPress={() => register()} />
-          </View>
+        <View style={{ marginTop: 30 }}>
+          <RoundedButton text="CONFIRMAR" onPress={() => register()} />
+        </View>
+
         </ScrollView>
-      </View>
 
-      <ModalPickImage
-        openGallery={pickImage}
-        openCamera={takePhoto}
-        modalUseState={modalVisible}
-        setModalUseState={setModalVisible}
-      />
+      </View>
     </View>
   );
 };
+
+
+
--- src/Presentation/views/register/ViewModel.tsx
+++ src/Presentation/views/register/ViewModel.tsx
@@ -1,106 +1,102 @@
-import React, { useState } from 'react';
-import { ApiDelivery } from '../../../Data/sources/remote/api/ApiDelivery';
-import { RegisterAuthUseCase } from '../../../Domain/useCases/auth/RegisterAuth';
-import { RegisterWithImageAuthUseCase } from '../../../Domain/useCases/auth/RegisterWithImageAuth';
+import React, { useState } from "react";
+import { ApiDelivery } from "../../../Data/sources/remote/api/ApiDelivery";
+import { RegisterAuthUseCase } from "../../../Domain/useCases/auth/RegisterAuth";
 import * as ImagePicker from 'expo-image-picker';
 
-const RegisterViewModel = () => {
+export const RegisterViewModel = () => {
 
-    const [errorMessage, setErrorMessage] = useState('');
-    const [values, setValues] = useState({
-        name: '',
-        lastname: '',
-        phone: '',
-        email: '',
-        image: '',
-        password: '',
-        confirmPassword: '',
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const [values, setValues] = useState({
+    name: "",
+    lastname: "",
+    phone: "",
+    email: "",
+    image: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [file, setfile] = useState<ImagePicker.ImagePickerAsset>()
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1
     });
-    const [file, setFile] = useState<ImagePicker.ImageInfo>()
 
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            quality: 1
-        });
-
-        if (!result.cancelled) {
-            onChange('image', result.uri);
-            setFile(result);
-        }
+    if (!result.canceled) {
+      onChange('image', result.assets[0].uri);
+      setfile(result.assets[0]);
     }
-    
-    const takePhoto = async () => {
-        let result = await ImagePicker.launchCameraAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            quality: 1
-        });
+  }
 
-        if (!result.cancelled) {
-            onChange('image', result.uri);
-            setFile(result);
-        }
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      onChange('image', result.assets[0].uri);
+      setfile(result.assets[0]);
     }
+  }
 
-    const onChange = (property: string, value: any) => {
-        setValues({ ...values, [property]: value })
+  const onChange = (property: string, value: any) => {
+    setValues({ ...values, [property]: value });
+  };
+
+  const register = async () => {
+    if(isValidForm()) {
+    const response = await RegisterAuthUseCase(values);
+    console.log('RESULT: '+ JSON.stringify(response));
     }
+  }
 
-    const register = async () => {
-        if (isValidForm()) {
-            // const response = await RegisterAuthUseCase(values);
-            const response = await RegisterWithImageAuthUseCase(values, file!);
-            console.log('RESULT: ' + JSON.stringify(response));        
-        }
+  const isValidForm = (): boolean => {
+    if(values.name === '') {
+      setErrorMessage('Ingresa tu nombre')
+      return false;
     }
-
-    const isValidForm = (): boolean => {
-        if (values.name === '') {
-            setErrorMessage('Ingresa tu nombre');
-            return false;
-        }
-        if (values.lastname === '') {
-            setErrorMessage('Ingresa tu apellido');
-            return false;
-        }
-        if (values.email === '') {
-            setErrorMessage('Ingresa tu correo electronico');
-            return false;
-        }
-        if (values.phone === '') {
-            setErrorMessage('Ingresa tu telefono');
-            return false;
-        }
-        if (values.password === '') {
-            setErrorMessage('Ingresa la contraseña');
-            return false;
-        }
-        if (values.confirmPassword === '') {
-            setErrorMessage('Ingresa la confirmacion de la contraseña');
-            return false;
-        }
-        if (values.password !== values.confirmPassword) {
-            setErrorMessage('Las contraseñas no coinciden');
-            return false;
-        }
-        if (values.image === '') {
-            setErrorMessage('Selecciona una imagen');
-            return false;
-        }
-
-        return true;
+    if(values.lastname === '') {
+      setErrorMessage('Ingresa tus apellidos')
+      return false;
+    }
+    if(values.email === '') {
+      setErrorMessage('Ingresa tu correo electrónico')
+      return false;
+    }
+    if(values.phone === '') {
+      setErrorMessage('Ingresa tu telefono')
+      return false;
+    }
+    if(values.password === '') {
+      setErrorMessage('Ingresa la contraseña')
+      return false;
+    }
+    if(values.confirmPassword === '') {
+      setErrorMessage('Ingresa la confirmación de la contraseña')
+      return false;
+    }
+    if(values.password !== values.confirmPassword) {
+      setErrorMessage('Las contraseñas no coinciden');
+      return false;
     }
 
-    return {
-        ...values,
-        onChange,
-        register,
-        pickImage,
-        takePhoto,
-        errorMessage
-    }
-}
+    return true;
+  }
+
+  return {
+    ...values,
+    onChange,
+    register,
+    pickImage,
+    takePhoto,
+    errorMessage
+  };
+};
 
 export default RegisterViewModel;
