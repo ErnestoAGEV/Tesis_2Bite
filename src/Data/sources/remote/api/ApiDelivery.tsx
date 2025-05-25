5fd0eb00275b3d84fd496b982d0c1cd2d634e7b7
ErnestoAGEV <vevelerne@gmail.com>
2025-05-25 11:37:47 -0600

    Intento de solucion de problema de versiones

--- src/Data/sources/remote/api/ApiDelivery.tsx
+++ src/Data/sources/remote/api/ApiDelivery.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ApiDelivery = axios.create({
-    baseURL: 'http://192.168.100.70/api',
+    baseURL: 'http://192.168.1.77/api',
     headers: {
         'Content-Type': 'application/json'
     }
@@ -9,7 +9,7 @@ const ApiDelivery = axios.create({
 
 
 const ApiDeliveryForImage = axios.create({
-    baseURL: 'http://192.168.100.70/api',
+    baseURL: 'http://192.168.1.77/api',
     headers: {
         'Content-Type': 'multipart/form-data',
         'accept': 'application/json'
